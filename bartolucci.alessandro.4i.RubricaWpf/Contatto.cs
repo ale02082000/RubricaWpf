1b6a4f866ef6e4a0af05ece083a2e0cfc589f15d
Alessandro Bartolucci <127590077+ale02082000@users.noreply.github.com>
2023-10-24 21:46:55 +0200

    Add files via upload

---
+++ bartolucci.alessandro.4i.RubricaWpf/Contatto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bartolucci.alessandro._4i.RubricaWpf
+{
+    internal class Contatto
+    {
+        private int _numero;
+        private string _cognome;
+
+        public int Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException();
+
+                _numero = value;
+            }
+        }
+
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefono { get; set; }
+        public string Citta { get; set; }
+        public string CAP { get; set; }
+
+        public string Cognome { get => _cognome; set => _cognome = value; }
+
+        public Contatto() { }
+
+        // Costruisce un Contatto, partendo da una riga CSV
+        public Contatto(string riga)
+        {
+            string[] campi = riga.Split(';');
+            if (campi.Length >= 4)
+            {
+                this.Nome = campi[0];
+                this.Cognome = campi[1];
+                this.Telefono = campi[2];
+                this.EMail = campi[3];
+            }
+        }
+
+        public Contatto(int numero, string nome, string cognome)
+        {
+            Numero = numero;
+            Nome = nome;
+            Cognome = cognome;
+        }
+    }
+}
---
+++ bartolucci.alessandro.4i.RubricaWpf/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bartolucci.alessandro._4i.RubricaWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Contatto[] Contatti = new Contatto[100];
+                for (int i = 0; i < Contatti.Length; i++)
+                    Contatti[i] = new Contatto();
+
+                int idx = 0;
+
+                StreamReader fin = new StreamReader("Dati.csv");
+                while (!fin.EndOfStream)
+                {
+                    string riga = fin.ReadLine();
+                    Contatto c = new Contatto(riga);
+
+                    Contatti[idx++] = c;
+                }
+
+                dgDati.ItemsSource = Contatti;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No no!!\n\n" + ex.Message);
+            }
+
+            /*
+            try
+            {
+                Contatto c = new Contatto();
+                c.Numero = 1;
+                c.Nome = "Maurizio";
+                c.Cognome = "Conti";
+                c.EMail = "maurizio.conti@ittsrimini.edu.it";
+                c.Telefono = "3337722";
+                c.CAP = "47923";
+
+                Contatti[0] = c;
+       
+                Contatto c1 = new Contatto { Numero = 2, Nome = "Riccardo", Cognome = "Bianchi" };
+                Contatti[1] = c1;
+
+                Contatto c2 = new Contatto (  2, "Antonio", "Vallone" );
+                Contatti[2] = c2;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("No no!!\n" + err.Message);
+            }
+            */
+
+
+        }
+    }
+}
