63ee4dd2ef824072db5e8e062bf6b2b8bd38addb
Alessandro Bartolucci <127590077+ale02082000@users.noreply.github.com>
2023-11-21 18:21:01 +0100

    Update MainWindow.xaml.cs

--- bartolucci.alessandro.4i.RubricaWpf/MainWindow.xaml.cs
+++ bartolucci.alessandro.4i.RubricaWpf/MainWindow.xaml.cs
@@ -25,77 +25,45 @@ namespace bartolucci.alessandro._4i.RubricaWpf
             InitializeComponent();
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
+       {
+             int idx = 0;
+            Contatto[] Contatti = new Contatto[100];
             try
-            {
-            const int MAX = 100;
-            StreamReader fin = new StreamReader("Dati.csv");
-
-                Contatto[] Contatti = new Contatto[MAX];
-               
-               
-               // for (int i = 0; i < Contatti.Length; i++)
-                int idx=0;
-                 foreach( Contatto c in Contatti)
-                    Contatti[idx++] = new Contatto();
-
-                 idx = 0;
-
-                while (!fin.EndOfStream)
                 {
-                if(idx<MAX)
-                    string riga = fin.ReadLine();
-                    Contatto c = new Contatto(riga);
-                    Contatti[idx++] = c;
+                
+                    StreamReader buffer = new StreamReader("Dati.csv");
+
+                for (int i = 0; i < Contatti.Length; i++)
+                    if (Contatti[i] == null)
+                        Contatti[i] = new Contatto();
+                        
+                idx = 0;
+                buffer.ReadLine();
+                while (!buffer.EndOfStream)
+                {
+                
+                    string row = buffer.ReadLine();
+                    Contatto contatto = new Contatto(row);
+                    Contatti[idx++] = contatto;
                 }
-
-                else 
-                    break;
-                    
-}
-
-        for(; idx< max; idx++){
-        Contatto c = new Contatto();
-        c.Numero = idx;
-        Contatti[idx] = c;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+            dgArray.ItemsSource = Contatti;
         }
-
         
-                    
-
-                dgDati.ItemsSource = Contatti;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("No no!!\n\n" + ex.Message\nalla);
-            }
-
-            /*
-            try
-            {
-                Contatto c = new Contatto();
-                c.Numero = 1;
-                c.Nome = "Maurizio";
-                c.Cognome = "Conti";
-                c.EMail = "maurizio.conti@ittsrimini.edu.it";
-                c.Telefono = "3337722";
-                c.CAP = "47923";
-
-                Contatti[0] = c;
-       
-                Contatto c1 = new Contatto { Numero = 2, Nome = "Riccardo", Cognome = "Bianchi" };
-                Contatti[1] = c1;
-
-                Contatto c2 = new Contatto (  2, "Antonio", "Vallone" );
-                Contatti[2] = c2;
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show("No no!!\n" + err.Message);
-            }
-            */
-
-
+        private void dgArray_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            Contatto? c = e.Row.Item as Contatto;
+            if (c != null)
+                if (c.Numero == 0) 
+                {
+                    e.Row.Background = Brushes.Red;
+                    e.Row.Foreground = Brushes.White;
+                }
         }
     }
 }
